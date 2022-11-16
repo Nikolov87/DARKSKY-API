3b7cc1c4391cb16276bc9a5a798328c81ba82004
Petar Nikolov <nikolov@Petars-MBP.broadband>
2022-11-16 17:43:49 +0000

    new add's in app.js

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 window.addEventListener('load', ()=> {
+
   let long;
   let lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+    );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let temperatureTimezone = document.querySelector(".location-timezone");
+
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
         long = position.coords.longitude;
         lat = position.coords.latitude;
 
+        const proxy = "https://cors-anywhere.herokuapp.com/";
         const api = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+
+        fetch(api)
+         .then(response => {
+          return response.json();
+         })
+          .then(data => {
+            const { temperature, summary } = data.currently;
+            //Set DOM Elements from the API
+            temperatureDegree.textContent = temperature;
+            temperatureDegree.textContent = summary;
+            locationTimezone.textContent = data.timezone;
+
+           
+          });
     });
-    //18:00min on the video...
   }
 });
